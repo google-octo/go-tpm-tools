b0b7bdf520ec9948b165e416c1234a64ebb1a357
Jiankun Lu <jiankun@google.com>
2023-11-28 23:07:40 +0800

    add cert negotiation server frame

--- launcher/certs/certs_negotiation_server.go
+++ launcher/certs/certs_negotiation_server.go
@@ -1,3 +1,18 @@
 package certs
 
+import (
+	"context"
+	"log"
+)
+
 // a grpc server that will handle incoming cert negotiation
+type server struct {
+	tcs TeeCertServer
+}
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) NegotiateCert(ctx context.Context, in *TeeCertNegotiateRequest) (*TeeCertNegotiateResponse, error) {
+	log.Printf("Received: %v", in.Cert)
+	log.Printf("%v", in.Token)
+	return &TeeCertNegotiateResponse{Cert: []byte("mycert"), Token: []byte("mytoken")}, nil
+}
